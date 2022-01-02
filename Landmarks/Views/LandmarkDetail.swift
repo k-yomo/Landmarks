5453473c08964d74c6255db68b9bf2df6c264b6c
k-yomo <kanji.yy@gmail.com>
2022-01-03 01:11:33 +0900

    Implement landmark list and detail views

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,52 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Kanji Yomoda on 2022/01/03.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .foregroundColor(.black)
+                HStack {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: ModalData().landmarks[0])
+    }
+}
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,43 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Kanji Yomoda on 2022/01/03.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var  modalData: ModalData
+    @State private var showFavroitesOnly =  false
+    
+    var filteredLandmarks: [Landmark] {
+        modalData.landmarks.filter { landmark in
+            (!showFavroitesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List() {
+                Toggle(isOn: $showFavroitesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
