909daa36bad291762e19c0fae8d3fa7083fe71f0
k-yomo <kanji.yy@gmail.com>
2022-01-02 22:24:56 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kanji Yomoda on 2022/01/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
